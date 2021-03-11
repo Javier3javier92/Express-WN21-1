6cd1a200e050613b2495aecdbdb1c870e5aa5ef7
Javier3javier92 <72465716+Javier3javier92@users.noreply.github.com>
2021-03-10 19:03:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
-
-app.listen(3000);
+mejia2javier@node-wn21:~/app$ cat index.js
+var http = require("http");
+http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello world');
+}).listen(process.env.PORT || 3000);
